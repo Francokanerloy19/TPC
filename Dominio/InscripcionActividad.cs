fe400eaf6750101eb401e49cf5ba702b42341f20
Franco Kaner Loy <franco@gmail.com>
2025-11-03 19:34:16 -0300

    Se crearon las clase de dominio

---
+++ Dominio/InscripcionActividad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class InscripcionActividad
+    {
+        public int IdInscripcionActividad { get; set; }
+        public int IdInscripcion { get; set; }
+        public int IdActividad { get; set; }
+        public decimal PrecioFinal { get; set; }
+    }
+}
